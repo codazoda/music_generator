7f9a98f6625ecbf8df09d0ad82dfb8f2609bf0bd
Joel Dare <joel@joeldare.com>
2016-02-05 11:39:39 -0700

    Updated the docblocks to provide complete documentation including examples. Replaced README.md with generated documentation.

--- MusicGenerator.js
+++ MusicGenerator.js
@@ -1,14 +1,84 @@
 /**
- * A class that generates music riffs, patterns, and progressions.
+ * 
+ * This is a Javascript library for generating random music riffs, patterns, and progressions.
+ *
+ * __Features__
+ *
+ * - Create a random rythm pattern of a length you specify
+ * - Create a random riff in a specified size from a specified music scale
+ * - Create a random chord progression using common chord rules
+ *
+ * __Examples__
+ *
+ * The following example creates an instance of the MusicGenerator class and generates
+ * a random chord progression.
+ *
+ * ```
+ * var generate = new MusicGenerator();
+ * var chords = generate.chordProgression(3);
+ * ```
+ *
+ * More examples are provided in the `NodeExample.js` file. If you have node installed 
+ * you can run it with the following command.
+ * 
+ * ```
+ * node NodeExample.js
+ * ```
+ * 
+ * __Documentation__
+ * 
+ * To generate documentation you'll need [jsdox](http://jsdox.org) installed and then you can create the
+ * documentation with the following command.
+ * 
+ * ```
+ * jsdox MusicGenerator.js --output ./
+ * mv MusicGenerator.md README.md
+ * ```
+ * 
+ * __More Information__
+ *
+ * I tried to stick to pure javascript for the library itself so that it could be 
+ * embedded in a web page, used in a node app, or any other place you can execute
+ * js code. I've done most of my testing, thus far, in node.js.
+ *
+ * Besides the typical hello world application, this is the first node.js code
+ * I've written. I tried to follow some js best practices but I don't know how
+ * well I've done.
+ * 
+ * __Future__
+ *
+ * There are a lot of things to do. It currently generates enough random patterns 
+ * to get me started and to kick start my imagination when I'm songwriting. The 
+ * patterns it generates don't typcially stand on their own and they aren't meant
+ * to; they are a starting point for your own creativity. Here's a bit of a wish list from me.
+ *
+ * - Create drum patterns
+ * - Maybe create more advanced rythm (picking note length)
+ * - Maybe generate an entire song given a pattern like 'verse, chorus, verse, chorus, chorus'.
+ *
+ * __License__
+ *
+ * It's early and I'm not sure yet. I'm considering all the open source licenses. If 
+ * you want to use it, please ask me.
+ * 
  */
 function MusicGenerator() {
 
 }
 
 /**
+ * 
+ * 
  * Generate a random rhythm pattern.
  *
  * The first position in the pattern is always true.
+ *
+ * __Example__
+ *
+ * ```
+ * var generate = new MusicGenerator();
+ * var rhythm = generate.rhythmPattern(8);
+ * ```
  *
  * @param  {number} notes  Number of notes to return (4 for Quarter, 8 for Eighth, etc.) Default: 8
  * @return {array}         Returns an array representing the pattern.
@@ -36,6 +106,14 @@ MusicGenerator.prototype.rhythmPattern = function(notes) {
  * Returns the list of selected notes in the specified scale. The scale could either be provided
  * or just the number of notes could be provided.
  *
+ * __Example__
+ *
+ * ```
+ * var generate = new MusicGenerator();
+ * var scale = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+ * var riff = generate.riffPattern(scale, 8);
+ * ```
+ * 
  * @param  {array}  scale  An array of notes in the scale.
  * @param  {number} notes  The number of notes to pick for this riff.
  * @return array           The full riff.
@@ -66,7 +144,16 @@ MusicGenerator.prototype.riffPattern = function(scale, notes) {
 }
 
 /**
- * Pick a random set of chords.
+ * Pick a random set of chords. These are returned as numbers that represent the
+ * numbered chords in any key. For example, [2,5,6] indicates the ii, v, and vi
+ * chords of any key. In the key of C those chords would be Dm, G, and Am.
+ * 
+ * __Example__
+ *
+ * ```
+ * var generate = new MusicGenerator();
+ * var chords = generate.chordProgression(3);
+ * ```
  * 
  * @param  {number} number The number of chords to pick.
  * @return {array}         The list of chords that were picked.
@@ -80,7 +167,7 @@ MusicGenerator.prototype.chordProgression = function (number) {
   // Pick an initial chord and push it onto the picked chords array
   var prevChord = Math.floor(Math.random() * 7) + 1;
   pickedChords.push(prevChord);
-  // These are some basic rules for chords (music theory). For example, if the previous 
+  // These are some basic rules for major chords (music theory). For example, if the previous 
   // chord was a ii (2), that chord typically moves to the v (5) or vii (7).
   var chordRules = {
     1: [1,2,3,4,5,6,7],
@@ -103,7 +190,7 @@ MusicGenerator.prototype.chordProgression = function (number) {
 }
 
 /**
- * Generate a drum pattern.
+ * TODO: Will eventually generate a drum pattern.
  * 
  * @return array The full drum pattern.
  */
--- README.md
+++ README.md
@@ -1,6 +1,49 @@
-# Music Generator Javascript Library
+# Global
 
-This is a Javascript library for generating random pieces of music.
+
+
+
+
+* * *
+
+### MusicGenerator() 
+
+This is a Javascript library for generating random music riffs, patterns, and progressions.
+
+__Features__
+
+- Create a random rythm pattern of a length you specify
+- Create a random riff in a specified size from a specified music scale
+- Create a random chord progression using common chord rules
+
+__Examples__
+
+The following example creates an instance of the MusicGenerator class and generates
+a random chord progression.
+
+```
+var generate = new MusicGenerator();
+var chords = generate.chordProgression(3);
+```
+
+More examples are provided in the `NodeExample.js` file. If you have node installed 
+you can run it with the following command.
+
+```
+node NodeExample.js
+```
+
+__Documentation__
+
+To generate documentation you'll need [jsdox](http://jsdox.org) installed and then you can create the
+documentation with the following command.
+
+```
+jsdox MusicGenerator.js --output ./
+mv MusicGenerator.md README.md
+```
+
+__More Information__
 
 I tried to stick to pure javascript for the library itself so that it could be 
 embedded in a web page, used in a node app, or any other place you can execute
@@ -10,31 +53,112 @@ Besides the typical hello world application, this is the first node.js code
 I've written. I tried to follow some js best practices but I don't know how
 well I've done.
 
-## Features
-
-- Create a random rythm pattern of a length you specify
-- Create a random riff in a specified size from a specified music scale.
-- Create a random chord progression using common chord rules.
-
-## Future
+__Future__
 
 There are a lot of things to do. It currently generates enough random patterns 
 to get me started and to kick start my imagination when I'm songwriting. The 
 patterns it generates don't typcially stand on their own and they aren't meant
-to do so. Here's a bit of a wish list from me.
+to; they are a starting point for your own creativity. Here's a bit of a wish list from me.
 
 - Create drum patterns
 - Maybe create more advanced rythm (picking note length)
 - Maybe generate an entire song given a pattern like 'verse, chorus, verse, chorus, chorus'.
 
-## NodeExample.js
-
-The NodeExample.js is a node.js example of how to use this library. If you have node installed 
-you can run it with the following command.
-
-    node NodeExample.js
-
-## License
+__License__
 
 It's early and I'm not sure yet. I'm considering all the open source licenses. If 
 you want to use it, please ask me.
+
+
+
+### rhythmPattern(notes) 
+
+Generate a random rhythm pattern.
+
+The first position in the pattern is always true.
+
+__Example__
+
+```
+var generate = new MusicGenerator();
+var rhythm = generate.rhythmPattern(8);
+```
+
+**Parameters**
+
+**notes**: `number`, Number of notes to return (4 for Quarter, 8 for Eighth, etc.) Default: 8
+
+**Returns**: `array`, Returns an array representing the pattern.
+
+
+### riffPattern(scale, notes) 
+
+Generate a random riff pattern for the provided scale. Each note can either stay the same, move up, 
+or move down the scale. If it moves up or down it can move one or two notes in that direction.
+
+Returns the list of selected notes in the specified scale. The scale could either be provided
+or just the number of notes could be provided.
+
+__Example__
+
+```
+var generate = new MusicGenerator();
+var scale = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+var riff = generate.riffPattern(scale, 8);
+```
+
+**Parameters**
+
+**scale**: `array`, An array of notes in the scale.
+
+**notes**: `number`, The number of notes to pick for this riff.
+
+**Returns**: , array           The full riff.
+
+
+### chordProgression(number) 
+
+Pick a random set of chords. These are returned as numbers that represent the
+numbered chords in any key. For example, [2,5,6] indicates the ii, v, and vi
+chords of any key. In the key of C those chords would be Dm, G, and Am.
+
+__Example__
+
+```
+var generate = new MusicGenerator();
+var chords = generate.chordProgression(3);
+```
+
+**Parameters**
+
+**number**: `number`, The number of chords to pick.
+
+**Returns**: `array`, The list of chords that were picked.
+
+
+### drumPattern() 
+
+TODO: Will eventually generate a drum pattern.
+
+**Returns**: , array The full drum pattern.
+
+
+### getRandomBool() 
+
+Returns a random boolean by picking a number between 0 and 1 without using
+Math.round(), which would give you a non-uniform distribution.
+
+
+
+
+* * *
+
+
+
+
+
+
+
+
+
+
